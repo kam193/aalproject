00b886e3333ee520c372192a631c735aa74355d5
Kamil Mankowski (kam193) <kam193@wp.pl>
2019-01-15 14:12:11 +0100

    Add q count

--- Tester/Tester.cpp
+++ Tester/Tester.cpp
@@ -1,12 +1,14 @@
+#include <cmath>
+
 #include "Tester.h"
 #include "../Generator/Generator.h"
 #include "../Solver/Solver.h"
 
 using namespace std;
 
-// std::chrono::steady_clock::time_point start = std::chrono::steady_clock::now();
-//     DFS(graph);
-// std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
+long double theoretic(long long int size){
+    return pow(32.0, size*size) * log2(size*size);
+}
 
 Tester::TestCase Tester::make_test(int size, int attempts){
     TestCase test = {size, 0};
@@ -37,6 +39,21 @@ void Tester::run_tests(int start_size, int step_size, int steps, int attepts)
         size += step_size;
     }
 
+    long long int median;
+    long long int median_time;
+    if (steps % 2 == 1)
+    {
+        median = tests[steps/2].size;
+        median_time = tests[steps/2].time;
+    }
+    else
+    {
+        median = (tests[steps/2].size + tests[steps/2 + 1].size) / 2;
+        median_time = (tests[steps/2].time + tests[steps/2 + 1].time) / 2;
+    }
+
+    long double theoretic_median = theoretic(median);
+
     cout << "  N (NxN) |    microseconds    | q(n) " << endl;
     cout << "------------------------------------" << endl;
     for (auto &test : tests)
@@ -44,6 +61,7 @@ void Tester::run_tests(int start_size, int step_size, int steps, int attepts)
         cout.width(10);
         cout << test.size << "|";
         cout.width(20);
-        cout << test.time << "|" << endl;
+        cout << test.time << "| ";
+        cout << (test.time * theoretic_median) / (theoretic(test.size) * median_time) << endl;
     }
 }
